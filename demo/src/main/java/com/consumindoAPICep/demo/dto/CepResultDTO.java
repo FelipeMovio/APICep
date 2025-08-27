1dc416919c845662bcb6d803c50ec8d0610ba7af
FelipeMovio <felipelsmovio@gmail.com>
2025-08-27 19:19:59 -0300

    mudanças no DTO

--- demo/src/main/java/com/consumindoAPICep/demo/dto/CepResultDTO.java
+++ demo/src/main/java/com/consumindoAPICep/demo/dto/CepResultDTO.java
@@ -1,6 +1,8 @@
 package com.consumindoAPICep.demo.dto;
 
 public record CepResultDTO(
+        // format de record para ser imultavel
+        // espelho do retorno que a viaCep(API que esta sendo consumida) nos tras
         String cep,
         String loradouro,
         String complemento,

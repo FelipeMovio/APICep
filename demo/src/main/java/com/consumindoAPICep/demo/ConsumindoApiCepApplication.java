ba989dc78e1adb6b6bd52f504c6c011579a9ea38
FelipeMovio <felipelsmovio@gmail.com>
2025-08-27 16:03:28 -0300

    Primeiros passos

---
+++ demo/src/main/java/com/consumindoAPICep/demo/ConsumindoApiCepApplication.java
@@ -0,0 +1,13 @@
+package com.consumindoAPICep.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumindoApiCepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumindoApiCepApplication.class, args);
+	}
+
+}

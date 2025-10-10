9ba382e570f73c429a686a898abeeea8a88aec1a
FelipeMovio <felipelsmovio@gmail.com>
2025-10-09 21:04:09 -0300

    create service

--- demo/src/main/java/com/consumindoAPICep/demo/Service/CepService.java
+++ demo/src/main/java/com/consumindoAPICep/demo/Service/CepService.java
@@ -1,8 +1,16 @@
-package com.consumindoAPICep.demo.Service;
+package com.consumindoAPICep.demo.service;
 
+import com.consumindoAPICep.demo.Entity.Cep;
 import com.consumindoAPICep.demo.repository.CepRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class CepService {
@@ -10,5 +18,34 @@ public class CepService {
     @Autowired
     private CepRepository cepRepository;
 
+    public List<Cep> buscarCep(String uf, String localidade, String logradouro) {
+        String url = UriComponentsBuilder
+                .fromHttpUrl("http://viacep.com.br/ws")
+                .pathSegment(uf, localidade, logradouro, "json")
+                .build()
+                .toUriString();
 
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<Cep[]> response = restTemplate.getForEntity(url, Cep[].class);
+
+        List<Cep> resultados = Arrays.asList(response.getBody());
+
+        // Salvar os resultados no banco se ainda não estiverem salvos
+        for (Cep dto : resultados) {
+            if (!cepRepository.existsByCep(dto.getCep())) {
+                Cep cep = new Cep();
+                cep.setCep(dto.getCep());
+                cep.setLogradouro(dto.getLogradouro());
+                cep.setComplemento(dto.getComplemento());
+                cep.setBairro(dto.getBairro());
+                cep.setLocalidade(dto.getLocalidade());
+                cep.setUf(dto.getUf());
+                cep.setDdd(dto.getDdd());
+
+                cepRepository.save(cep);
+            }
+        }
+
+        return resultados;
+    }
 }
--- demo/src/main/java/com/consumindoAPICep/demo/repository/CepRepository.java
+++ demo/src/main/java/com/consumindoAPICep/demo/repository/CepRepository.java
@@ -1,8 +1,9 @@
 package com.consumindoAPICep.demo.repository;
 
-import com.consumindoAPICep.demo.Entity.CepResultDTO;
+
+import com.consumindoAPICep.demo.Entity.Cep;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CepRepository extends JpaRepository<CepResultDTO,Long> {
+public interface CepRepository extends JpaRepository<Cep,Long> {
     boolean existsByCep(String cep);
 }
